debd37b0e2501bedbc1d3df16ca59bb7df195a0f
Juan Batiz-Benet <juan@benet.ai>
2014-05-11 00:28:27 -0700

    schema fix

--- index.js
+++ index.js
@@ -4,5 +4,5 @@ module.exports = new transformer.Type({
   // @context and type filled in automatically.
   'id': 'js-date',
   'description': 'General JS Date',
-  'schema': "string"
+  'schema': "Date"
 });
